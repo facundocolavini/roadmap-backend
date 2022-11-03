e7de7bb0cfc68cc226047a028f31d784f5ee293a
fcolavini <fcolavini@southernhorizon.tech>
2022-11-03 16:56:19 -0300

    app clima

--- bases-node/practicas-node/05-clima-app/app.ts
+++ bases-node/practicas-node/05-clima-app/app.ts
@@ -1,8 +1,8 @@
 import * as dotenv from 'dotenv';
 import { colors } from './config';
 import { inquirerMenu, listCitys, pause, readInput } from './helpers';
+import { City, ResClimateCity } from './interfaces/city';
 import { Searchs } from './models';
-import { City, ClimateCity } from './interfaces/city';
 
 dotenv.config()
 
@@ -10,6 +10,7 @@ const main = async () => {
     // Opciones del menu
     let opt: number;
     let searchs = new Searchs()
+
     do {
         opt = await inquirerMenu();
         switch (opt) {
@@ -17,31 +18,41 @@ const main = async () => {
                 //Mostra mensaje
                 let place = await readInput('Ingrese la ciudad:');
                 //Buscar Lugar
-                let searchPlaces = await searchs.searchCity(place);
+                let searchPlaces: City[] = await searchs.searchCity(place);
                 // Seleccione  el lugar
                 let idPlaceSelected = await listCitys(searchPlaces);
+                if( idPlaceSelected === '0') continue;
+
+
                 let placeSelected: City | undefined = searchPlaces?.find(p => (p?.id === idPlaceSelected))
 
                 // Obtener temperatura del lugar
-                if (placeSelected !== undefined) {
-                    const { lat, lng, name } = placeSelected
-                    let climateCity = await searchs.getClimateCity(placeSelected);
-                    console.log(climateCity, 'ALGO')
+                if(placeSelected !== undefined){
+                    const { lat, lng, name } = placeSelected;
+                    // Guardar en DB
+                    searchs.addToHistorial(name)  
+                    let { temp, temp_min, temp_max , descr } = await searchs.getClimateCity(placeSelected);
+                    // console.log(await searchs.getClimateCity(placeSelected))
+    
                     // Mostrar resultados 
                     console.log(colors.green(`\nInformaión de la ciudad:\n`));
                     console.log(colors.green(`Ciudad: ${colors.warn(name)}`));
                     console.log(colors.green(`Latitud: ${colors.warn(lat)}`));
                     console.log(colors.green(`Longitud: ${colors.warn(lng)}`));
-                    console.log(colors.green(`Temperatura:`));
-                    console.log(colors.green(`Min: `));
-                    console.log(colors.green(`Max:`));
+                    console.log(colors.green(`Temperatura: ${colors.warn(temp)}`));
+                    console.log(colors.green(`Min: ${colors.warn(temp_min)}`));
+                    console.log(colors.green(`Max: ${colors.warn(temp_max)}`));
+                    console.log(colors.green(`Como esta el clima: ${colors.warn(descr)}`));
                     // console.log(searchs);
                 }
-
                 break;
 
             case 2:
                 console.log(colors.green('\nHistorial de busqueda\n'));
+                searchs.history.forEach((place,i)=>{    
+                    const idx = i + 1;
+                    console.log(`${colors.green(idx)}. ${place}`)
+                })
                 break;
         }
         if (opt !== 0) await pause();
--- bases-node/practicas-node/05-clima-app/models/searchs.ts
+++ bases-node/practicas-node/05-clima-app/models/searchs.ts
@@ -1,11 +1,13 @@
+import fs from 'fs';
 import axios from 'axios';
-import { City, CityResp, WeatherParamsClimate, WeatherResponse, ClimateCity } from '../interfaces/city';
+import { City, CityResp, ClimateCityWeather, ResClimateCity, Weather, WeatherParamsClimate } from '../interfaces/city';
 
 export class Searchs {
     public history: Array<string> = [];
-
+    private readonly dbPath = './db/database.json';
     constructor() {
         // TODO: Leer DB si existe
+        this.readDB()
 
     }
 
@@ -20,11 +22,11 @@ export class Searchs {
 
     get getParamsOpenWeatherMap() {
         return {
-            'APPID': process.env.OPENWEATHERMAP_KEY
+            'APPID': process.env.OPENWEATHERMAP_KEY,
         }
     }
 
-    async searchCity(place: string) {// Es asyncrono por que se conecta a una db o pega a una api externa o algo externo
+    async searchCity(place: string = ''):Promise<City[]> {// Es asyncrono por que se conecta a una db o pega a una api externa o algo externo
 
         const URL: string = `https://api.mapbox.com/geocoding/v5/mapbox.places`;
         const SEARCH_STRING = `/${place}.json`;
@@ -41,44 +43,127 @@ export class Searchs {
                     name: place?.place_name,
                     lng: place?.center[0],
                     lat: place?.center[1]
-                }
+                } as City
             })
-            return resSearch
+            return resSearch as City[]
         }
         catch (error) {
             console.log(error);
-            return []
+            return [] as any
 
         }
     }
 
-    async getClimateCity({ lat, lng, lang = 'es' }: WeatherParamsClimate) {
+    async getClimateCity({ lat, lng, lang = 'es' }: WeatherParamsClimate): Promise<ResClimateCity> {
         const URL: string = `https://api.openweathermap.org/data/2.5/weather`;
 
         try {
             const instance = axios.create({
                 baseURL: URL,
-                params: this.getParamsOpenWeatherMap
+                params: {
+                    ...this.getParamsOpenWeatherMap,
+                    lat,
+                    lng,
+                    lang
+                }
+
+            })
+            // Forma de fernando
+            const resp = await instance.get(`?lat=${lat}&lon=${lng}&lang=${lang}&units=metric`);
+            const  { main, weather } = resp?.data;
+
+            return {
+                descr: weather[0]?.description,
+                temp: main?.temp,
+                temp_min: main?.temp_min,
+                temp_max: main?.temp_max
+            }  as ResClimateCity
+            // MI forma
+/*             const resp = await instance.get(`?lat=${lat}&lon=${lng}&lang=${lang}&units=metric`);
+            const respTemp = [(resp.data.main)].find((p) => {
+            
+               return {
+                    temp: p.main?.temp,
+                    temp_min: p.main?.temp_min,
+                    temp_max: p.main?.temp_max,
+                    // descr: p.weather[0]?.description
+                } 
 
             })
 
-            const resp = await instance.get(`?lat=${lat}&lon=${lng}&lang=${lang}`);
-            [resp.data]?.find((p: WeatherResponse) => ({
-                p
-            }))
-            /*          {
-                         temp: p.temp,
-                         temp_min: p.temp_min,
-                         temp_max: p.temp_max
-                     } as ClimateCity */
+            const respWeather = resp?.data.weather?.find((w: Weather) => {
+                
+                return {
+
+                     descr: w?.description
+                 } 
+ 
+            })
 
 
+           return {
+                ...respTemp,
+                descr: respWeather?.description
+           } 
+ */
         } catch (error) {
             console.log(error);
-            return []
+            return [] as any
 
         }
+    }
+    
+    addToHistorial(place:string = ''){
+        // Grabar los buscados
+        if(this.history.includes(place.toLocaleLowerCase()))return;
 
+        this.history.unshift(place)
+        this.saveInDb()
+        //Prevenir duplicado       
+    }
+
+    saveInDb(){
+        const payload = {
+            history:this.history
+        }
+
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload))
+    }
+
+    readDB(){
+       
+        if (!fs.existsSync(this.dbPath)) { // Si mi db existe
+            return null;
+        }
+    
+        const dataString = fs.readFileSync(this.dbPath, { encoding: 'utf-8' })//Encoding para que no me regrese los bytes
+        // Parsea de string a mi array 
+        const dataArray = JSON.parse(dataString);
+        
+        return dataArray;
+        
+    }
+
+    readCitysOnDB(arrCitys = []) {
+        // if (!arrTasks) {
+        //     throw new Error("No hay tareas en la DB");
+        // }
+
+
+        // arrTasks.map((task: Task) => {
+        //     this.tasksList = {
+        //         ...this.tasksList, // Todas las tareas anteriores
+        //         [task.id]: task,
+        //     }
+        // })
+        // console.log(`Tareas en DB:\n`)
+        // console.log(this.tasksList)
+
+        //Forma de fernando 
+
+        // arrCitys.forEach((city) => {
+        //     this.history[city?.id] = city;
+        // })
 
     }
 }
